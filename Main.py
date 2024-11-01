be6beb20acb6d68258e746054b5f95a64d47d578
KareemTheSecond <kareemrhassani@gmail.com>
2024-11-01 12:45:13 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,4 +5,14 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-from model import MyDataSet, CNN_FF
+from Train_Evaluate import Train,evaluate_model 
+
+
+directory_train = r"TRAIN_FOLDER_PATH"
+directory_val = r"VALIDATION_FOLDER_PATH"
+directory_test = r"TEST_FOLDER_PATH"
+
+Train(directory_train,directory_val,directory_test)
+
+
+
