c8f3bfb05915684ddb433866c57af2c0a40600a6
KareemTheSecond <kareemrhassani@gmail.com>
2024-11-01 12:26:25 +0200

    Create Models.py

---
+++ Models.py
@@ -0,0 +1,28 @@
+class MyDataSet(Dataset): 
+    def __init__(self, directoryOfDataset, transform=None): 
+        self.data = ImageFolder(directoryOfDataset, transform=transform)
+        
+    def __getitem__(self, index): 
+        return self.data[index]
+    
+    def __len__(self):
+        return len(self.data)
+
+class NetClassifier(nn.Module): 
+    def __init__(self): 
+        super().__init__() 
+        self.conv1 = nn.Conv2d(3, 6, 5) # 3 * 128 * 128 => conv1 :6 * 124 * 124 => pool: 6 * 62 * 62 => conv2: 16*58*58 => pool2: 16*29*29 
+        self.pool = nn.MaxPool2d(2, 2) # 
+        self.conv2 = nn.Conv2d(6, 16, 5) #
+        self.fc1 = nn.Linear(16*29*29, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 53)
+
+    def forward(self, x): 
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
