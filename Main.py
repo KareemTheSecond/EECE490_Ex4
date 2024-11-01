ee31c1cb0a645428b23cab6842748daec06ca55b
KareemTheSecond <kareemrhassani@gmail.com>
2024-11-01 12:49:53 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
+from Train_Evaluate import Train, evaluate_model
+from model import MyDataSet, CNN_FF
 import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
-from Train_Evaluate import Train,evaluate_model 
-
 
 directory_train = r"TRAIN_FOLDER_PATH"
 directory_val = r"VALIDATION_FOLDER_PATH"
 directory_test = r"TEST_FOLDER_PATH"
+transform = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
 
-Train(directory_train,directory_val,directory_test)
+dataset_train = MyDataSet(directory_train, transform=transform)
+dataset_val = MyDataSet(directory_val, transform=transform)
+dataset_test = MyDataSet(directory_test, transform=transform)
 
+model = CNN_FF()  
+Train(directory_train, directory_val, directory_test) 
+val_accuracy = evaluate_model(DataLoader(dataset_val, batch_size=32, shuffle=False))
+print(f'Validation Accuracy: {val_accuracy * 100:.2f}%')
 
-
+test_accuracy = evaluate_model(DataLoader(dataset_test, batch_size=32, shuffle=False))
+print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
