add57ecae436538e89384fdfe51d7733565787ad
KareemTheSecond <kareemrhassani@gmail.com>
2024-11-01 12:30:29 +0200

    Create Train.py

---
+++ Train.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from model import MyDataSet, NetClassifier
+
