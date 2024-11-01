6e5f1b7d4cf1dfa46e2d83bc6d44a0b9fde5b096
KareemTheSecond <kareemrhassani@gmail.com>
2024-11-01 13:22:18 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,17 @@
 import torch
-from Train_Evaluate import Train, evaluate_model
-from model import MyDataSet, CNN_FF
+from Train_Evaluate import Train_CNN_FF, evaluate_model
+from Models import MyDataSet, CNN_FF
 import torchvision.transforms as transforms
 
 directory_train = r"TRAIN_FOLDER_PATH"
 directory_val = r"VALIDATION_FOLDER_PATH"
 directory_test = r"TEST_FOLDER_PATH"
 
-model, dataloader_val, dataloader_test  = Train(directory_train, directory_val, directory_test) 
+model, dataloader_val, dataloader_test  = Train_CNN_FF(directory_train, directory_val, directory_test) 
 val_accuracy = evaluate_model(model, dataloader_val)
 print(f'Validation Accuracy: {val_accuracy * 100:.2f}%')
 
 test_accuracy = evaluate_model(model, dataloader_test)
 print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
+
+
